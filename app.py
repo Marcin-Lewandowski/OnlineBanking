ec9fc8fb03710077c38b2d1bd2c22106fa9f688c
Marcin Lewandowski <telfordmarcin@gmail.com>
2023-12-29 18:56:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, flash, request, url_for, redirect, ses
 from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
-from forms.forms import LoginForm, TransferForm, AddCustomerForm, CreateTransactionForm, DeleteUserForm, EditUserForm, ChangePasswordForm, AddRecipientForm, DDSOForm
+from forms.forms import AddCustomerForm, DeleteUserForm, ChangePasswordForm, AddRecipientForm
 from datetime import timedelta, date
-from routes.transfer import transfer_bp, login_bp
+from routes.transfer import transfer_bp, login_bp, ddso_bp, create_transaction_bp, edit_profile_bp
+from routes.my_routes import grocery1_bp, grocery2_bp, grocery3_bp, grocery4_bp, gas_bp, power_bp, petrol_bp, clothes_bp, water_bp, add_customer_bp
 from models.models import Users, Transaction, db, Recipient, DDSO
 from functools import wraps
 
@@ -29,13 +30,23 @@ def create_app():
     # ... Rejestracja Blueprintów, inne konfiguracje ...
     app.register_blueprint(transfer_bp)
     app.register_blueprint(login_bp)
-
+    app.register_blueprint(ddso_bp)
+    app.register_blueprint(create_transaction_bp)
+    app.register_blueprint(edit_profile_bp)
+    app.register_blueprint(add_customer_bp)
+    app.register_blueprint(grocery1_bp)
+    app.register_blueprint(grocery2_bp)
+    app.register_blueprint(grocery3_bp)
+    app.register_blueprint(grocery4_bp)
+    app.register_blueprint(gas_bp)
+    app.register_blueprint(power_bp)
+    app.register_blueprint(petrol_bp)
+    app.register_blueprint(clothes_bp)
+    app.register_blueprint(water_bp)
     return app
 
 
-
 app = create_app()
-  
   
 def initialize_app():
     
@@ -43,8 +54,6 @@ def initialize_app():
         db.create_all()
         create_sample_user()
         
-        
-         
         
 @app.before_request
 def initialize_database():
@@ -83,14 +92,12 @@ def index():
 
 
 
-
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash('Zostałeś wylogowany.', 'success')
     return redirect(url_for('index'))
-
 
 
 
@@ -114,7 +121,6 @@ def make_payment():
 
     return render_template('make_payment.html', user=current_user, all_transactions=user_transactions, last_transaction=last_transaction, all_recipients=user_recipients)
     
-
 
 
 
@@ -143,245 +149,7 @@ def add_recipient():
 
 
  
-        
     
-    
-    
-def handle_grocery_transaction(amount, recipient_id, description, description2, description3):
-    # Twoja istniejąca logika sprawdzania salda i transakcji
-    #user_transactions = Transaction.query.filter_by(user_id=current_user.id).all()
-    
-    # Pobierz ostatnią transakcję kupującego, aby sprawdzić jego saldo
-    last_transaction = Transaction.query.filter_by(user_id=current_user.id).order_by(Transaction.id.desc()).first()
-    
-    if current_user.id == recipient_id:
-        flash('You cannot buy your own products and services !!!', 'danger') 
-        return redirect(url_for('online_shop'))
-        
-        
-    if last_transaction.balance < amount:
-            flash('Insufficient funds.', 'danger')
-            return redirect(url_for('online_shop'))
-        
-    else:    
-        # Logika wykonania przelewu
-        try:
-            # Przygotowanie wspólnych danych dla transakcji
-            transaction_date = date.today()
-            
-            # Aktualizacja salda nadawcy
-            sender_balance = last_transaction.balance - amount
-            new_sender_transaction = Transaction(user_id=current_user.id, 
-                                                transaction_date=transaction_date,
-                                                transaction_type='DD',
-                                                sort_code=last_transaction.sort_code,
-                                                account_number=last_transaction.account_number,
-                                                transaction_description=description,
-                                                debit_amount=amount,
-                                                credit_amount = 0,
-                                                balance=sender_balance)
-            db.session.add(new_sender_transaction)
-            
-            # Znajdź ostatnią transakcję odbiorcy - sklepu FreshFood
-            last_recipient_transaction = Transaction.query.filter_by(user_id=recipient_id).order_by(Transaction.id.desc()).first()
-            
-            recipient_balance = last_recipient_transaction.balance + amount
-            
-            new_recipient_transaction = Transaction(user_id=recipient_id,
-                                                        transaction_date=transaction_date,
-                                                        transaction_type='FPI',
-                                                        sort_code=last_recipient_transaction.sort_code,
-                                                        account_number=last_recipient_transaction.account_number,
-                                                        transaction_description=description2,
-                                                        debit_amount = 0,
-                                                        credit_amount=amount,
-                                                        balance=recipient_balance)
-            db.session.add(new_recipient_transaction)
-            
-            # Zatwierdzenie zmian w bazie danych
-            db.session.commit()
-            
-            flash(f'{description3} purchase completed successfully!', 'success')
-            return redirect(url_for('online_shop'))
-        
-        except Exception as e:
-                print(7)
-                db.session.rollback()
-                flash('An error occurred. Purchase failed.', 'danger')
-                return render_template('online_shop.html', user=current_user, last_transaction=last_transaction)
-            
-            
- 
-    
-    
-    
-    
-    
-    
-@app.route('/grocery1', methods=['GET', 'POST'])
-@login_required
-def grocery1():
-    amount = 65
-    recipient_id = 16
-    description = 'Grocery - Dairy purchase'
-    description2 = 'Grocery - Dairy sale'
-    description3 = 'Dairy'
-    
-    return handle_grocery_transaction(amount, recipient_id, description, description2, description3)
-
-   
-@app.route('/grocery2', methods=['GET', 'POST'])
-@login_required
-def grocery2():
-    amount = 50
-    recipient_id = 16 
-    description = 'Grocery - Fruits & veggies purchase'
-    description2 = 'Grocery - Fruits & veggies sale'
-    description3 = 'Fruits & veggies'
-    
-    return handle_grocery_transaction(amount, recipient_id, description, description2, description3)    
-
-@app.route('/grocery3', methods=['GET', 'POST'])
-@login_required
-def grocery3():
-    amount = 45
-    recipient_id = 16  
-    description = 'Grocery - Fish & meat purchase'
-    description2 = 'Grocery - Fish & meat sale'
-    description3 = 'Fish & meat'
-    
-    return handle_grocery_transaction(amount, recipient_id, description, description2, description3)
-
-
-@app.route('/grocery4', methods=['GET', 'POST'])
-@login_required
-def grocery4():
-    amount = 25
-    recipient_id = 16  
-    description = 'Grocery - Bread and rolls purchase'
-    description2 = 'Grocery - Bread and rolls sale'
-    description3 = 'Bread & rolls'
-    
-    return handle_grocery_transaction(amount, recipient_id, description, description2, description3)
-
-
-@app.route('/gas', methods=['GET', 'POST'])
-@login_required
-def gas():
-    amount = 50
-    recipient_id = 18  
-    description = 'Gas purchase'
-    description2 = 'Gas sale'
-    description3 = 'Gas'
-    
-    return handle_grocery_transaction(amount, recipient_id, description, description2, description3)
-
-
-@app.route('/power', methods=['GET', 'POST'])
-@login_required
-def power():
-    amount = 60
-    recipient_id = 18  
-    description = 'Electric purchase'
-    description2 = 'Electric sale'
-    description3 = 'Electric '
-    
-    return handle_grocery_transaction(amount, recipient_id, description, description2, description3)
-
-
-@app.route('/water', methods=['GET', 'POST'])
-@login_required
-def water():
-    amount = 110
-    recipient_id = 17  
-    description = 'Water purchase'
-    description2 = 'Water sale'
-    description3 = 'Water'
-    
-    return handle_grocery_transaction(amount, recipient_id, description, description2, description3)
-
-@app.route('/clothes', methods=['GET', 'POST'])
-@login_required
-def clothes():
-    amount = 150
-    recipient_id = 22
-    description = 'Clothes purchase'
-    description2 = 'Clothes sale'
-    description3 = 'Clothes'
-    
-    return handle_grocery_transaction(amount, recipient_id, description, description2, description3)
-
-
-@app.route('/petrol', methods=['GET', 'POST'])
-@login_required
-def petrol():
-    amount = 120
-    recipient_id = 21
-    description = 'Petrol purchase'
-    description2 = 'Petrol sale'
-    description3 = 'Petrol'
-    
-    return handle_grocery_transaction(amount, recipient_id, description, description2, description3)
-
-
-
-
-
-@app.route('/direct_debit_standing_orders', methods=['GET', 'POST'])
-@login_required
-def ddso():
-    
-    user_transactions = Transaction.query.filter_by(user_id=current_user.id).all()
-    user_dd_so = DDSO.query.filter_by(user_id=current_user.id).all()
-    last_transaction = Transaction.query.filter_by(user_id=current_user.id).order_by(Transaction.id.desc()).first()
-    
-    form = DDSOForm()
-    
-    if form.validate_on_submit():
-        
-        confirm_password = form.confirm_password.data
-        
-        # Sprawdź, czy hasło jest poprawne
-        if not current_user.check_password(confirm_password):
-            print(3)
-            flash('Invalid password.', 'danger')
-            return redirect(url_for('dashboard'))
-        
-        # Sprawdź, czy odbiorca istnieje, po username
-        recipient_exist = Users.query.filter_by(username=form.recipient.data).first()
-        
-        
-        if not recipient_exist:
-            print(4)
-            flash('Recipient account not found.', 'danger')
-            return redirect(url_for('dashboard')) 
-        
-        
-        # Tworzenie nowego zlecenia na podstawie danych z formularza
-        new_dd_so = DDSO(
-                        user_id=current_user.id,  
-                        recipient=form.recipient.data,
-                        reference_number=form.reference_number.data,
-                        amount=form.amount.data,
-                        next_payment_date=form.next_payment_date.data,
-                        transaction_type=form.transaction_type.data,
-                        frequency=form.frequency.data)
-        
-        db.session.add(new_dd_so)
-        db.session.commit()
-        flash('New Direct Debit / Standing Order added successfully!')
-        return redirect(url_for('dashboard'))  # Przekierowanie do strony głównej lub innej strony
-
-    return render_template('ddso.html', form=form, user=current_user, all_transactions=user_transactions, all_dd_so=user_dd_so,  last_transaction=last_transaction)
-    
-
-
-
-
-
-
-
-
 @app.route('/online_shop', methods=['GET', 'POST'])
 @login_required
 def online_shop():
@@ -434,85 +202,7 @@ def admin_dashboard_cm():
 
     # Renderuj szablon, przekazując dane o użytkownikach
     return render_template('admin_dashboard_cm.html', all_users=all_users, all_transactions=all_transactions)
-    
-
-
-@app.route('/create_transaction', methods=['GET', 'POST'])      # nie ma zabezpieczenia , sprawdzic czy istnieje już taki sam sort code i account number
-@admin_required
-def create_transaction():
-    
-    all_users = Users.query.all()
-    all_transactions = Transaction.query.all()
-    form = CreateTransactionForm()
-    print(1)
-
-    # Ładowanie użytkowników do wyboru
-    form.user_id.choices = [(user.id, user.username) for user in Users.query.all()]
-
-    if form.validate_on_submit():
-        # Tworzenie obiektu transakcji
-        print(2)
-        new_transaction = Transaction(user_id=form.user_id.data,
-                                        transaction_date=form.transaction_date.data,
-                                        transaction_type=form.transaction_type.data,
-                                        sort_code=form.sort_code.data,
-                                        account_number=form.account_number.data,
-                                        transaction_description=form.transaction_description.data,
-                                        debit_amount=form.debit_amount.data,
-                                        credit_amount=form.credit_amount.data,
-                                        balance=form.balance.data)
-
-        db.session.add(new_transaction)
-        db.session.commit()
-
-        flash('Transaction created successfully!', 'success')
-        print(3)
-        return redirect(url_for('admin_dashboard_cm'))  # Przekierowanie do odpowiedniej strony
-
-    print(4)
-    return render_template('admin_dashboard_cm.html', all_users=all_users, form=form, all_transactions=all_transactions)                   
-
-
-
-# Dodaj trasę do obsługi formularza dodawania klienta
-@app.route('/add_customer', methods=['GET', 'POST'])
-@admin_required
-def add_customer():
-
-    form = AddCustomerForm(request.form)
-    
-    if form.validate_on_submit():
-        # Uzyskaj dane z formularza
-        username = form.username.data
-        password = form.password.data
-        email = form.email.data
-        password_hash = generate_password_hash(password, method='pbkdf2:sha256')
-        
-        # Sprawdź, czy użytkownik o podanym username lub email już istnieje
-        existing_user = Users.query.filter((Users.username == username) | (Users.email == email)).first()
-
-        if existing_user:
-            
-            # Użytkownik o podanym username lub email już istnieje
-            flash('User with given username or email already exists.', 'error')
-            return render_template('user_exist.html')
-        else:
-            # Utwórz nowego użytkownika i dodaj go do bazy danych
-            
-            role = form.role.data
-            country = form.country.data
-            email = form.email.data
-            phone_number = form.phone_number.data
-
-            new_user = Users(username=username, password_hash=password_hash, role=role, email=email, phone_number=phone_number, country=country)
-            db.session.add(new_user)
-            db.session.commit()
-
-            flash('User added successfully.', 'success')
-            return redirect(url_for('admin_dashboard_cm'))
-    
-    # Jeśli formularz nie jest poprawny, ponów renderowanie strony z błędami
-    #return render_template('admin_dashboard', form=form)
+                      
 
 
 
@@ -539,34 +229,6 @@ def delete_user():
         return redirect(url_for('admin_dashboard_cm'))
 
     return render_template('admin_dashboard_cm.html', form=form, all_users=all_users)
-
-
-
-@app.route('/edit_profile', methods=['GET', 'POST'])
-@login_required  # Upewnij się, że tylko zalogowani użytkownicy mogą edytować profil
-def edit_profile():
-    form = EditUserForm()
-
-    if form.validate_on_submit():
-        current_user.email = form.email.data
-        current_user.phone_number = form.phone_number.data
-        current_user.country = form.country.data
-        
-        db.session.commit()
-        flash('Your profile has been updated.')
-        return redirect(url_for('dashboard'))
-
-    elif request.method == 'GET':
-        form.email.data = current_user.email
-        form.phone.data = current_user.phone
-        form.country.data = current_user.country
-        # Załaduj inne pola
-    else:
-        print(form.errors) 
-
-    user_transactions = Transaction.query.filter_by(user_id=current_user.id).all()
-    return render_template('dashboard.html', user=current_user, all_transactions=user_transactions)
-
 
 
 
@@ -605,7 +267,6 @@ def change_password(user_id):
 
 
 
-
 @app.route('/branch_locator', methods=['GET', 'POST'])
 def branch_locator():
     return render_template('branch_locator.html')
@@ -630,8 +291,7 @@ def contact_us():
 
 
 '''
-
-593
+640 --> 300
 '''
 
 if __name__ == "__main__":
