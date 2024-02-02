03549bb95a13e88114b0dfa95eb0e06d6677e8b6
Marcin Lewandowski <telfordmarcin@gmail.com>
2024-02-02 19:24:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from routes.transfer import transfer_bp, login_bp, ddso_bp, create_transaction_b
 from routes.my_routes import grocery1_bp, grocery2_bp, grocery3_bp, grocery4_bp, gas_bp, power_bp, petrol_bp, clothes_bp, water_bp, add_customer_bp
 from routes.my_routes_hc import send_query_bp, process_query_bp, read_message_bp, send_message_for_query_bp, send_message_for_message_bp, delete_messages_for_query_bp
 from routes.my_routes_hc import delete_query_confirmation_bp, show_statement_for_customer_bp, edit_customer_information_bp
+from routes.my_routes_statement import download_transactions_bp, download_transactions_csv_bp
+from routes.my_routes_admin import transactions_filter_bp, reports_and_statistics_bp
 from models.models import Users, Transaction, db, Recipient, DDSO, SupportTickets, LockedUsers
 from functools import wraps
 from urllib.parse import quote
@@ -120,9 +122,6 @@ def process_ddso_payments():
                             print('Direct debit sended successful!')
                             
                             
-                            
-                         
-                            
                         except Exception as e:
                             db.session.rollback()
                             # Wydrukuj komunikat o błędzie i pełny stos wywołań
@@ -132,9 +131,6 @@ def process_ddso_payments():
         else:
             print("Brak oczekujących płatności.")
             return  # Zakończenie funkcji jeśli nie ma płatności do przetworzenia
-        
-        
-        
         
         
         
@@ -194,6 +190,12 @@ def create_app():
     app.register_blueprint(delete_query_confirmation_bp)
     app.register_blueprint(show_statement_for_customer_bp)
     app.register_blueprint(edit_customer_information_bp)
+    
+    app.register_blueprint(download_transactions_bp)
+    app.register_blueprint(download_transactions_csv_bp)
+    
+    app.register_blueprint(transactions_filter_bp)
+    app.register_blueprint(reports_and_statistics_bp)
     
     return app
 
@@ -330,17 +332,6 @@ def register():
 
 
 
-def plot_to_html_img(plt):
-    # Zapisz wykres w buforze pamięci
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-
-    # Koduj jako base64
-    image_base64 = base64.b64encode(buf.getvalue()).decode('utf-8').replace('\n', '')
-    buf.close()
-
-    return f'<img src="data:image/png;base64,{image_base64}"/>'
 
 
 @app.route('/admin_dashboard', methods=['GET', 'POST'])
@@ -507,40 +498,7 @@ def transaction_management():
 
     
     
-@app.route('/transactions_filter', methods=['GET', 'POST'])
-@login_required
-@admin_required  
-def transactions_filter():
-    all_transactions = Transaction.query.all()
-    
-    if request.method == 'POST':
-        user_id = request.form.get('user_id')
-        date_from = request.form.get('date_from')
-        date_until = request.form.get('date_until')
-        transaction_type = request.form.get('transaction_type')
 
-        query = Transaction.query
-
-        # Filtracja po ID użytkownika, jeśli podano
-        if user_id:
-            query = query.filter(Transaction.user_id == user_id)
-
-        # Filtracja po zakresie dat
-        if date_from:
-            query = query.filter(Transaction.transaction_date >= date_from)
-        if date_until:
-            query = query.filter(Transaction.transaction_date <= date_until)
-
-        # Filtracja po typie transakcji, jeśli wybrano inny niż 'all'
-        if transaction_type and transaction_type != 'all':
-            query = query.filter(Transaction.transaction_type == transaction_type)
-
-        # Pobranie wyników
-        transactions = query.all()
-
-        return render_template('transaction_management.html', transactions=transactions, all_transactions=all_transactions)
-
-    return render_template('transaction_management.html', transactions=transactions)
     
 
 
@@ -671,416 +629,6 @@ def delete_user():
 
 
 
-
-
-# Funkcja zliczająca ile logów o danym poziomie jest zapisanych w pliku z logami
-def count_log_levels(file_path):
-    log_levels = {"INFO": 0, "ERROR": 0, "WARNING": 0, "CRITICAL": 0}
-
-    with open(file_path, "r", encoding="utf-8") as file:
-        for line in file:
-            if "INFO" in line:
-                log_levels["INFO"] += 1
-            elif "ERROR" in line:
-                log_levels["ERROR"] += 1
-            elif "WARNING" in line:
-                log_levels["WARNING"] += 1
-            elif "CRITICAL" in line:
-                log_levels["CRITICAL"] += 1
-
-    return log_levels
-
-
-
-
-
-
-
-
-
-
-
-def create_transactions_pdf(transactions, username):
-    buffer = BytesIO()
-    pdf = SimpleDocTemplate(buffer, pagesize=letter)
-    
-    
-    # Styl dla paragrafu
-    styles = getSampleStyleSheet()
-    
-    # Tekst do umieszczenia przed tabelą
-    
-    intro_text = f"Poniżej znajduje się lista wszystkich transakcji dokonanych na Twoim koncie, {username}: "
-    intro = Paragraph(intro_text, styles['Normal'])
-    epmty_space = Paragraph("<br/><br/>", styles['Normal'])  # Pusty paragraf jako odstęp
-
-
-    data = [["Date", "Type", "Description", "Debit amount", "Credit amount", "Balance"]]  # Nagłówki kolumn
-    for transaction in transactions:
-        data.append([str(transaction.transaction_date), str(transaction.transaction_type), str(transaction.transaction_description), str(transaction.debit_amount), str(transaction.credit_amount), str(transaction.balance)])
-
-    # Tworzenie tabeli
-    table = Table(data)
-
-    # Styl tabeli (opcjonalnie)
-    style = TableStyle([
-        ('BACKGROUND', (0,0), (-1,0), colors.grey),
-        ('TEXTCOLOR',(0,0),(-1,0),colors.whitesmoke),
-        ('ALIGN',(0,0),(-1,-1),'CENTER'),
-        ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
-        ('BOTTOMPADDING', (0,0), (-1,0), 12),
-        ('BACKGROUND',(0,1),(-1,-1),colors.beige),
-        ('GRID', (0,0), (-1,-1), 1, colors.black)
-    ])
-    table.setStyle(style)
-
-    # Dodawanie tabeli do elementów PDF
-    
-    elems = [intro, epmty_space, table]
-    pdf.build(elems)
-
-    buffer.seek(0)
-    return buffer
-
-
-
-
-
-
-
-@app.route('/download_transactions/<int:user_id>')
-@login_required
-def download_transactions(user_id):
-    # Sprawdzenie, czy zalogowany użytkownik ma uprawnienia do pobrania transakcji
-    if current_user.id != user_id and not current_user.is_admin:
-        abort(403)
-
-    # Pobranie transakcji użytkownika z bazy danych
-    transactions = Transaction.query.filter_by(user_id=user_id).all()
-
-    # Tworzenie PDF
-    
-    buffer = create_transactions_pdf(transactions, current_user.username)
-
-    # Utworzenie odpowiedzi HTTP z plikiem PDF
-    response = make_response(buffer.getvalue())
-    buffer.close()
-    response.headers['Content-Disposition'] = 'attachment; filename=transactions.pdf'
-    response.mimetype = 'application/pdf'
-
-    return response
-
-
-
-
-
-def save_transactions_to_csv(transactions, filename):
-    with open(filename, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        
-        # Nagłówki kolumn
-        writer.writerow(["Date", "Type", "Description", "Debit amount", "Credit amount", "Balance"])
-
-        # Zapisz dane transakcji
-        for transaction in transactions:
-            writer.writerow([transaction.transaction_date, transaction.transaction_type, transaction.transaction_description, transaction.debit_amount, transaction.credit_amount, transaction.balance])
-            
-            
-@app.route('/download_transactions_csv')
-@login_required
-def download_transactions_csv():
-    # Pobierz dane transakcji (możesz tu dodać filtr na transakcje aktualnego użytkownika)
-    transactions = Transaction.query.filter_by(user_id=current_user.id).all()
-
-    # Tworzenie nazwy pliku
-    filename = f"transactions_{current_user.username}.csv"
-
-    # Zapisz dane do CSV
-    save_transactions_to_csv(transactions, filename)
-
-    # Utworzenie odpowiedzi z plikiem CSV
-    return send_file(filename, as_attachment=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-            
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@app.route('/reports_and_statistics', methods=['GET', 'POST'])
-@login_required
-@admin_required
-def reports_and_statistics():
-    
-    # Pobranie danych o rolach użytkowników
-    roles_count = Users.query.with_entities(Users.role, func.count(Users.role)).group_by(Users.role).all()
-
-    # Tworzenie DataFrame
-    roles_df = pd.DataFrame(roles_count, columns=['Role', 'Count'])
-
-    # Tworzenie wykresu kołowego
-    fig, ax = plt.subplots(figsize=(4, 3))  # Utworzenie figury i osi
-    ax.pie(roles_df['Count'], labels=roles_df['Role'], autopct='%1.1f%%', startangle=140)
-    ax.set_title('Rozkład ról użytkowników w systemie')
-
-    # Zmiana koloru tła
-    ax.set_facecolor('black')  # Możesz wybrać dowolny kolor
-    fig.patch.set_facecolor('lightgrey')  # Zmiana koloru tła figury
-    
-    # Konwertuj wykres na HTML img
-    chart1 = plot_to_html_img(plt)
-    
-    
-    
-    
-    # Pobieranie danych o narodowościach użytkownków
-    countries_count = Users.query.with_entities(Users.country, func.count(Users.country)).group_by(Users.country).all()
-    
-    #Tworzenie dataframe
-    countries_df = pd.DataFrame(countries_count, columns = ['Country', 'Count'])
-    
-    # Tworzenie wykresu kołowego
-    fig, ax = plt.subplots(figsize=(4, 3))  # Utworzenie figury i osi
-    ax.pie(countries_df['Count'], labels=countries_df['Country'], autopct='%1.1f%%', startangle=140)
-    ax.set_title('Rozkład narodowości użytkowników w systemie')
-
-    # Zmiana koloru tła
-    ax.set_facecolor('black')  # Możesz wybrać dowolny kolor
-    fig.patch.set_facecolor('lightgrey')  # Zmiana koloru tła figury
-    
-    # Konwertuj wykres na HTML img
-    chart2 = plot_to_html_img(plt)
-    
-    
-    
-    # Podzapytanie do wyodrębnienia unikalnych numerów referencyjnych z ich priorytetami
-    subquery = (db.session.query(SupportTickets.reference_number, SupportTickets.priority)
-                .distinct(SupportTickets.reference_number)
-                .subquery())
-
-    # Zapytanie główne - grupowanie i zliczanie priorytetów na podstawie unikalnych numerów referencyjnych
-    tickets_count = (db.session.query(subquery.c.priority, func.count(subquery.c.priority))
-                    .group_by(subquery.c.priority)
-                    .all())
-
-    # Reszta kodu pozostaje bez zmian
-    tickets_df = pd.DataFrame(tickets_count, columns=['Priority', 'Count'])
-
-    fig, ax = plt.subplots(figsize=(6, 3))
-    ax.pie(tickets_df['Count'], labels=tickets_df['Priority'], autopct='%1.1f%%', startangle=140)
-    ax.set_title('Rozkład ilości wiadomości z określonym priorytetem w systemie')
-    ax.set_facecolor('black')
-    fig.patch.set_facecolor('lightgrey')
-
-    chart3 = plot_to_html_img(plt)
-    
-    
-    
-    
-    
-    
-    # Pobieranie danych o narodowościach użytkownków
-    transaction_types_count = Transaction.query.with_entities(Transaction.transaction_type, func.count(Transaction.transaction_type)).group_by(Transaction.transaction_type).all()
-    
-    #Tworzenie dataframe
-    transaction_types_df = pd.DataFrame(transaction_types_count, columns = ['Transaction type', 'Count'])
-    
-    # Tworzenie wykresu kołowego
-    fig, ax = plt.subplots(figsize=(6, 3))  # Utworzenie figury i osi
-    ax.pie(transaction_types_df['Count'], labels=transaction_types_df['Transaction type'], autopct='%1.1f%%', startangle=140)
-    ax.set_title('Rozkład typów tranzakcji w systemie tranzakcji')
-
-    # Zmiana koloru tła
-    ax.set_facecolor('black')  # Możesz wybrać dowolny kolor
-    fig.patch.set_facecolor('lightgrey')  # Zmiana koloru tła figury
-    
-    # Konwertuj wykres na HTML img
-    chart4 = plot_to_html_img(plt)
-    
-    
-    
-    
-    
-    
-    # Zapytanie do obliczenia średniej wartości transakcji dla każdego typu
-    avg_transactions = (db.session.query(Transaction.transaction_type,func.avg(Transaction.debit_amount + Transaction.credit_amount).label('average_value'))
-                    .filter((Transaction.debit_amount + Transaction.credit_amount) <= 10000).group_by(Transaction.transaction_type).all())
-
-    # Tworzenie DataFrame z wyników
-    transactions_df = pd.DataFrame(avg_transactions, columns=['TransactionType', 'AverageValue'])
-
-    # Tworzenie wykresu słupkowego
-    fig, ax = plt.subplots(figsize=(8, 6))
-    transactions_df.plot(kind='bar', x='TransactionType', y='AverageValue', ax=ax, color='skyblue')
-
-    ax.set_title('Średnia Wartość Transakcji dla Każdego Typu Transakcji')
-    ax.set_xlabel('Typ Transakcji')
-    ax.set_ylabel('Średnia Wartość')
-    plt.xticks(rotation=45)
-    
-    # Dodawanie wartości liczbowych na wykresie
-    for bar in ax.patches:
-        ax.annotate(format(bar.get_height(), '.2f'), 
-                (bar.get_x() + bar.get_width() / 2, 
-                    bar.get_height()), ha='center', va='center',
-                size=10, xytext=(0, 8),
-                textcoords='offset points')
-    
-    # Konwertuj wykres na HTML img
-    chart5 = plot_to_html_img(plt)
-    
-    
-    response_times = []
-    
-    # Znajdź unikalne numery referencyjne
-    unique_reference_numbers = (SupportTickets.query.with_entities(SupportTickets.reference_number).distinct().all())
-
-    # Wyświetl ilość unikalnych numerów referencyjnych
-    print(f"Ilość unikalnych numerów referencyjnych: {len(unique_reference_numbers)}")
-
-    # Wyświetl numery referencyjne
-    for ref_number in unique_reference_numbers:
-        print(ref_number[0])
-
-        dates = (db.session.query(SupportTickets.created_at)
-            .filter(SupportTickets.reference_number == ref_number[0])  # Użyj [0], aby pobrać numer referencyjny
-            .order_by(asc(SupportTickets.created_at))
-            .limit(2)  # Ograniczenie do pierwszych dwóch rekordów
-            .all())
-        
-        
-
-        # Obliczenie różnicy czasu
-        if len(dates) == 2:
-            first_record, second_record = dates[0][0], dates[1][0]  # Zakładając, że dates zawiera daty
-            time_difference = second_record - first_record
-            response_times.append(time_difference)
-            print("Czas, jaki upłynął między rekordami:", time_difference)
-        else:
-            print("Nie znaleziono wystarczającej liczby rekordów.")
-    
-    if response_times:
-        total_time = sum(response_times, timedelta())  # Sumowanie timedelta
-        average_time_seconds = total_time.total_seconds() / len(response_times)  # Średni czas w sekundach
-
-        # Obliczenie godzin i minut
-        hours, remainder = divmod(average_time_seconds, 3600)
-        minutes, _ = divmod(remainder, 60)
-
-        # Wyświetlenie średniego czasu w godzinach i minutach
-        print("Średni czas odpowiedzi: {} godzin {} minut".format(int(hours), int(minutes)))
-    else:
-        print("Nie znaleziono wystarczającej liczby rekordów do obliczenia średniego czasu odpowiedzi.")
-    
-    
-    if response_times:
-        average_time_seconds = total_time.total_seconds() / len(response_times)
-        hours, remainder = divmod(average_time_seconds, 3600)
-        minutes, _ = divmod(remainder, 60)
-        average_response_time = "{} godzin {} minut".format(int(hours), int(minutes))
-    else:
-        average_response_time = "Brak danych"
-    
-    
-    
-    # Grupowanie wiadomości według priorytetu i liczenie ich
-    priority_counts = (SupportTickets.query
-                       .with_entities(SupportTickets.priority, func.count(SupportTickets.reference_number.distinct()))
-                       .group_by(SupportTickets.priority)
-                       .all())
-
-    # Inicjalizacja zmiennych dla każdego priorytetu
-    normal_count = high_count = urgent_count = 0
-
-    # Przypisanie wyników do odpowiednich zmiennych
-    for priority, count in priority_counts:
-        if priority == 'normal':
-            normal_count = count
-        elif priority == 'high':
-            high_count = count
-        elif priority == 'urgent':
-            urgent_count = count
-    total_count = normal_count + high_count + urgent_count
-    
-    
-    # Wywołanie funkcji zliczającej lofi
-    
-    log_file_path = 'app.log'  # Ścieżka do Twojego pliku logów
-    log_counts = count_log_levels(log_file_path)
-    
-    
-    # Tworzenie DataFrame dla wyjresu logów w pliku app.log
-    #log_counts = count_log_levels(log_file_path) # załóżmy, że log_counts to twoje dane
-    log_counts_df = pd.DataFrame(list(log_counts.items()), columns=['Log Level', 'Count'])
-
-    # Tworzenie wykresu kołowego
-    fig, ax = plt.subplots(figsize=(6, 3))  # Utworzenie figury i osi
-    ax.pie(log_counts_df['Count'], labels=log_counts_df['Log Level'], autopct='%1.1f%%', startangle=140)
-    ax.set_title('Rozkład typów logów w systemie')
-
-    # Zmiana koloru tła
-    ax.set_facecolor('black')  # Możesz wybrać dowolny kolor
-    fig.patch.set_facecolor('lightgrey')  # Zmiana koloru tła figury
-
-    # Konwertowanie wykresu na HTML img
-    chart6 = plot_to_html_img(plt)
-    
-    
-    return render_template('reports_and_statistics.html', chart1 = chart1, chart2 = chart2, chart3 = chart3, chart4=chart4, chart5=chart5, average_response_time=average_response_time, 
-                           normal_count = normal_count, high_count = high_count, urgent_count = urgent_count, total_count=total_count, log_counts = log_counts, chart6 = chart6)
-    
-
-
 @app.route('/products_and_service_management', methods=['GET', 'POST'])
 @login_required
 @admin_required
@@ -1162,7 +710,7 @@ def contact_us():
     return render_template('contact_us.html')
 
 '''
-665 linii 
+1160 linii -> 1013 -> 735
 '''
 
 if __name__ == "__main__":
